87ec5379f72bda092a70aed9d941b7a9cfe86c1c
Iraz Irfan <Irazirfan@gmail.com>
2019-05-04 18:50:44 +0600

    _RiderLayout created

--- SP1.Chalao.Web/Controllers/ParkingZoneController.cs
+++ SP1.Chalao.Web/Controllers/ParkingZoneController.cs
@@ -5,10 +5,10 @@ namespace SP1.Chalao.Web.Controllers
     public class ParkingZoneController : Controller
     {
         // GET: ParkingZone
-        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
+        
     }
 }
